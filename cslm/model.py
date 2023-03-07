36c9c1a924b2efbce5ba3c97a51d5c209d199206
Shaleen Kalsi <shaleen.kalsi@gmail.com>
2023-03-07 15:36:36 +0000

    Revert "changes for dynamic-data-augmentation"

--- cslm/model.py
+++ cslm/model.py
@@ -5,10 +5,8 @@ from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
     get_linear_schedule_with_warmup,
-    BertModel,
 )
 from torch.optim import AdamW
-import numpy as np
 import torch.nn.functional as F
 
 class LightningModel(LightningModule):
@@ -17,24 +15,9 @@ class LightningModel(LightningModule):
 
         self.config = config
         self.save_hyperparameters()
-        self.auto_config = AutoConfig.from_pretrained(self.config.upstream_model, num_labels=num_labels)
-        #self.model = AutoModelForSequenceClassification.from_pretrained(self.config.upstream_model, config=self.auto_config)
-        self.model = BertModel.from_pretrained(self.config.upstream_model, config=self.auto_config)
-        #print(self.model)
-        #print("*******************************************************************************************")
-        #bert_params = list(self.model.named_parameters())
-        #x = self.model(0, 0)[0][-1]
-        #print("x:", x)
-        #print(bert_params[17][1])
+        self.auto_config = AutoConfig.from_pretrained(self.config.upstream_model, num_labels=config.num_classes)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.config.upstream_model, config=self.auto_config)
         self.loss_fn = torch.nn.CrossEntropyLoss()
-        input_dim = 768
-        self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(input_dim, 128),
-            torch.nn.ReLU(),
-            torch.nn.Linear(128, num_labels)
-        )
-        #print(self.classifier)
-        self.mixup_type = self.config.mixup_type
         if config.num_classes == 2:
             task = 'binary'
         else:
@@ -45,53 +28,31 @@ class LightningModel(LightningModule):
             for name, param in self.model.named_parameters():
                 if 'classifier' not in name: # classifier layer
                     param.requires_grad = False
-        
-    def basic_forward(self, input_ids, attention_mask, labels):
-        return self.model(input_ids=input_ids, attention_mask=attention_mask)
-    
-    def mixup_forward(self, input_ids_x, input_ids_mixup_x, attention_mask_x, attention_mask_mixup_x, lam):
-        x = self.model(input_ids=input_ids_x, attention_mask=attention_mask_x)[1]
-        mixup_x = self.model(input_ids=input_ids_mixup_x, attention_mask=attention_mask_mixup_x)[1]
-        
-        x = lam*x + (1-lam)*mixup_x
 
-        logits = self.classifier(x)
-        return logits
-    
-    def forward(self, input_ids_x, input_ids_mixup_x, attention_mask_x, attention_mask_mixup_x, labels_x, labels_mixup_x, lam):
-        return self.mixup_forward(input_ids_x, input_ids_mixup_x, attention_mask_x, attention_mask_mixup_x, lam)
+    def forward(self, input_ids, attention_mask):
+        return self.model(input_ids=input_ids, attention_mask=attention_mask) 
 
     def training_step(self, batch, batch_idx):
         # batch
-        print("start training")
-        input_ids_x = batch['input_ids_x']
-        input_ids_mixup_x = batch['input_ids_mixup_x']
-        labels_x = batch['labels_x']
-        labels_mixup_x = batch['labels_mixup_x']
-        attention_mask_x = batch['attention_mask_x']
-        attention_mask_mixup_x = batch['attention_mask_mixup_x']
-
-        alpha = 0.5
-        lam = np.random.beta(alpha, alpha)
-
-        #construct the mixup label
-        #labels_x = torch.stack(labels_x)
-        #labels_mixup_x = torch.stack(labels_mixup_x)
-        labels_x = lam*labels_x + (1-lam)*labels_mixup_x
-
+        input_ids = batch['input_ids']
+        labels = batch['labels']
+        attention_mask = batch['attention_mask']
         #token_type_ids = batch['token_type_ids']
         # fwd
-        logits = self(input_ids_x, input_ids_mixup_x, attention_mask_x, attention_mask_mixup_x, labels_x, labels_mixup_x, lam)
+        outputs = self(input_ids, attention_mask) # outputs are from a linear FC, NOT SOFTMAX
+        # Tuple containing loss[in this case loss is not calculated], logits, hidden states and attentions,
+        # since loss is None, only one element in tuple
+        logits = outputs[0] 
         preds = logits.view(-1, self.config.num_classes)
         pred_probs = F.softmax(preds, dim=1)
         # accuracy
         # float tensor of shape (N, C, ..), if preds is a floating point we apply torch.argmax along the C dimension 
         # to automatically convert probabilities/logits into an int tensor.
-        acc = self.accuracy(preds, torch.argmax(labels_x, dim=1))
+        acc = self.accuracy(preds, torch.argmax(labels, dim=1))
         # loss
         # torch.nn.CrossEntropyLoss() combines nn.LogSoftmax() and nn.NLLLoss() in one single class.
         # Therefore, you should not use softmax before.
-        loss = self.loss_fn(preds, labels_x)
+        loss = self.loss_fn(preds, labels)
 
         return {'loss': loss, 'acc': acc} # For backprop
 
@@ -104,20 +65,18 @@ class LightningModel(LightningModule):
 
 
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        outputs = self(input_ids, attention_mask)
 
-        input_ids_x = batch['input_ids_x']
-        labels_x = batch['labels_x']
-        attention_mask_x = batch['attention_mask_x']
-
-        outputs = self.basic_forward(input_ids_x, attention_mask_x, labels_x)
-
-        logits = self.classifier(outputs[1])
+        labels = batch["labels"]
+        logits = outputs[0]
         preds = logits.view(-1, self.config.num_classes)
         pred_probs = F.softmax(preds, dim=1)
         # accuracy
-        acc = self.accuracy(preds, torch.argmax(labels_x, dim=1))
+        acc = self.accuracy(preds, torch.argmax(labels, dim=1))
         # loss
-        loss = self.loss_fn(preds, labels_x)
+        loss = self.loss_fn(preds, labels)
 
         return {"val_loss": loss, "val_acc": acc}
 
@@ -128,18 +87,19 @@ class LightningModel(LightningModule):
         self.log('val/acc',val_acc, on_step=False, on_epoch=True, prog_bar=True)
 
     def test_step(self, batch, batch_idx, dataloader_idx=0):
-        input_ids_x = batch['input_ids']
-        attention_mask_x = batch['attention_mask']
-        labels_x = batch['labels_x']
-        outputs = self.basic_forward(input_ids_x, attention_mask_x, labels_x)
-        logits = self.classifier(outputs[1])
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        outputs = self(input_ids, attention_mask)
+
+        labels = batch["labels"]
+        logits = outputs[0]
 
         preds = logits.view(-1, self.config.num_classes)
         pred_probs = F.softmax(preds, dim=1)
         # accuracy
-        acc = self.accuracy(preds, torch.argmax(labels_x, dim=1))
+        acc = self.accuracy(preds, torch.argmax(labels, dim=1))
         # loss
-        loss = self.loss_fn(preds, labels_x)
+        loss = self.loss_fn(preds, labels)
 
         return {"test_loss": loss, "test_acc": acc}
 
